8ada20e5b00a644ebda3cc149e8cf879e193504e
Upeksha Wickramasinghe <upekshawikcramasinghe2005@gmail.com>
2026-05-14 17:32:38 +0530

    Add weather bot to fetch weather by city

---
+++ weather_bot.py
@@ -0,0 +1,6 @@
+import requests
+print('Input your city and find your weather')
+input=('Enter your city name: ')
+url = f"http://wttr.in/{city}?format=3"
+response=requests.get(url)
+print(response)
